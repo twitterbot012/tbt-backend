4079b45da0f953155571ef505e2a5e3b744f409e
facugirardi22@gmail.com <facugirardi22@gmail.com>
2025-04-04 13:53:40 -0300

    fix

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def start_tweet_fetcher():
                         break
 
                     print("⏳ Esperando hasta 30 segundos antes de la próxima búsqueda...")
-                    for _ in range(30):  # Esperar en intervalos de 1 segundo para detectar el stop
+                    for _ in range(30):  
                         if fetching_event.is_set():
                             break
                         time.sleep(1)
